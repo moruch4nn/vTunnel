af8fe69a2fdcac6ff14ac696f8337f9625df606f
moruch4nn <moruch4nn@gmail.com>
2023-04-22 09:29:43 +0900

    ⭐pingパススルー時に合計プレイヤー数が反映されないバグを修正

--- server/src/main/kotlin/dev/mr3n/vtunnel/tunnel/PingManager.kt
+++ server/src/main/kotlin/dev/mr3n/vtunnel/tunnel/PingManager.kt
@@ -2,17 +2,23 @@ package dev.mr3n.vtunnel.tunnel
 
 import com.velocitypowered.api.proxy.server.RegisteredServer
 import com.velocitypowered.api.proxy.server.ServerPing
+import dev.mr3n.vtunnel.VTunnel
 
 private val cachedPingsInfo = mutableMapOf<String, CachedPingInfo>()
 
 class CachedPingInfo(val registeredServer: RegisteredServer) {
     private var lastUpdate = System.currentTimeMillis()
 
-    private var cachedPing = registeredServer.ping().get()
+    private var cachedPing = this.getNonCachedInfo()
+
+    private fun getNonCachedInfo(): ServerPing = this.registeredServer.ping().get().asBuilder()
+        .onlinePlayers(VTunnel.SERVER.playerCount)
+        .maximumPlayers(VTunnel.SERVER.configuration.showMaxPlayers)
+        .build()
 
     fun get(): ServerPing {
         if(System.currentTimeMillis() - this.lastUpdate > 1000) {
-            this.cachedPing = registeredServer.ping().get()
+            this.cachedPing = this.getNonCachedInfo()
             this.lastUpdate = System.currentTimeMillis()
         }
         return cachedPing
