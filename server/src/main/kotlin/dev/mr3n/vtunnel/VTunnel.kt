d5f9f3c95a5faa20d03ceda1888b7b9bec184b1f
moruch4nn <moruch4nn@gmail.com>
2023-06-25 11:32:54 +0900

    ⭐️ユーザーのIPアドレスを隠すように変更

--- server/src/main/kotlin/dev/mr3n/vtunnel/VTunnel.kt
+++ server/src/main/kotlin/dev/mr3n/vtunnel/VTunnel.kt
@@ -2,6 +2,7 @@ package dev.mr3n.vtunnel
 
 import com.velocitypowered.api.event.PostOrder
 import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.event.connection.PreLoginEvent
 import com.velocitypowered.api.event.player.KickedFromServerEvent
 import com.velocitypowered.api.event.player.PlayerChooseInitialServerEvent
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
@@ -11,6 +12,7 @@ import com.velocitypowered.api.proxy.ProxyServer
 import com.velocitypowered.api.proxy.server.RegisteredServer
 import dev.mr3n.vtunnel.tunnel.cachedPingInfo
 import dev.mr3n.vtunnel.tunnel.startTunnelingAllocator
+import java.net.InetAddress
 import java.net.InetSocketAddress
 import java.util.*
 import java.util.logging.Logger
@@ -20,6 +22,16 @@ import kotlin.jvm.optionals.getOrNull
 
 @Plugin(id = "vtunnel")
 class VTunnel @Inject constructor(val server: ProxyServer, logger: Logger) {
+
+    fun coverAddress(conn: Any) {
+        val inetAddress = InetAddress.getByName("0.0.0.0")
+        val inetSocketAddress = InetSocketAddress(inetAddress, 0)
+        val mcConn = conn::class.java.getDeclaredField("connection")
+            .also { it.isAccessible = true }.get(conn)
+        mcConn::class.java.getDeclaredField("remoteAddress")
+            .also { it.isAccessible = true }.set(mcConn, inetSocketAddress)
+    }
+
     @Subscribe(order = PostOrder.FIRST)
     fun on(event: ProxyInitializeEvent) {
         thread { startTunnelingAllocator() }
@@ -32,7 +44,16 @@ class VTunnel @Inject constructor(val server: ProxyServer, logger: Logger) {
     }
 
     @Subscribe(order = PostOrder.FIRST)
+    fun on(event: PreLoginEvent) {
+        val conn = event.connection
+        val delegate = conn::class.java.getDeclaredField("delegate")
+            .also { it.isAccessible = true }.get(conn)
+        this.coverAddress(delegate)
+    }
+
+    @Subscribe(order = PostOrder.FIRST)
     fun on(event: ProxyPingEvent) {
+        coverAddress(event.connection)
         val virtualHostStr = event.connection.virtualHost.map(InetSocketAddress::getHostString).orElse("").lowercase(Locale.ROOT)
         (forcedHosts(virtualHostStr)?:tryServer())?.cachedPingInfo()?.let(event::setPing)
     }
